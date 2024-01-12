b0158509ffa90f8b53f3eb421d7312d7084cc594
Nandalala Infotech <94165593+nandalalainfotech@users.noreply.github.com>
2024-01-12 10:19:51 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  images: {
+    unoptimized: true,
+  },
+  basePath:
+    process.env.NODE_ENV === "production"
+      ? `/react/molla/demo-${process.env.NEXT_PUBLIC_DEMO}`
+      : "",
+  trailingSlash: true,
+  env: {
+    PUBLIC_URL:
+      process.env.NODE_ENV === "production"
+        ? `/react/molla/demo-${process.env.NEXT_PUBLIC_DEMO}/`
+        : "/",
+    APP_URL:
+      process.env.NODE_ENV === "production"
+        ? "https://d-themes.com/react/molla/"
+        : "http://d-themes.com/react/molla",
+    // APP_URL: process.env.NODE_ENV === 'production' ? 'https://d-themes.com/react/molla/' : 'http://localhost/'
+  },
+};
